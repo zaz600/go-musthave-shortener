d8cc963a0e634b353f7179acf6692139ac0f6faa
Dmitriy Denisov <zaz600@gmail.com>
2021-11-15 23:02:45 +0300

    first

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,88 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+const listenAddr = ":8080"
+
+type ShortenerHandler struct {
+	mu  sync.RWMutex
+	db  map[int64]string
+	seq int64
+}
+
+func (s *ShortenerHandler) getUrl(idStr string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return "", false
+	}
+	longUrl, ok := s.db[id]
+	return longUrl, ok
+}
+
+func (s *ShortenerHandler) putUrl(longUrl string) (int64, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, err := url.Parse(longUrl)
+	if err != nil {
+		return 0, err
+	}
+	s.seq++
+	s.db[s.seq] = longUrl
+	return s.seq, err
+}
+
+func (s *ShortenerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		id := strings.TrimPrefix(r.URL.Path, "/")
+		if longUrl, ok := s.getUrl(id); ok {
+			http.Redirect(w, r, longUrl, http.StatusTemporaryRedirect)
+		} else {
+			http.Error(w, "url not found", http.StatusBadRequest)
+		}
+	case http.MethodPost:
+		if strings.TrimPrefix(r.URL.Path, "/") != "" {
+			http.Error(w, "invalid url, use /", http.StatusBadRequest)
+			break
+		}
+
+		bytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "invalid request params", http.StatusBadRequest)
+			break
+		}
+		id, err := s.putUrl(string(bytes))
+		if err != nil {
+			http.Error(w, "invalid request params", http.StatusBadRequest)
+		}
+		_, _ = fmt.Fprintf(w, "%d", id)
+
+	default:
+		http.Error(w, "Only GET/POST requests are allowed!", http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	server := http.Server{
+		Addr: listenAddr,
+		Handler: &ShortenerHandler{
+			db:  make(map[int64]string),
+			seq: 1,
+		},
+	}
+
+	log.Fatalln(server.ListenAndServe())
+}
