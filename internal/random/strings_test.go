4879114c3fad1994d7ba6f5ad1209cf1fb0c34da
Dmitriy Denisov <zaz600@gmail.com>
2021-12-26 18:04:20 +0300

    Тесты

--- internal/random/strings_test.go
+++ internal/random/strings_test.go
@@ -6,11 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRandString(t *testing.T) {
+func TestString(t *testing.T) {
 	assert.Empty(t, String(-1))
 	assert.Empty(t, String(0))
 	assert.Len(t, String(1), 1)
 	assert.Len(t, String(10), 10)
 
 	assert.NotEqual(t, String(10), String(10))
+}
+
+func TestUserID(t *testing.T) {
+	assert.Len(t, UserID(), 24)
+	assert.NotEqual(t, UserID(), UserID())
 }
