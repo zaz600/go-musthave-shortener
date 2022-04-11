27d0a7be4836ffd57ee8270409f9972fc48b5977
Dmitriy Denisov <zaz600@gmail.com>
2022-04-11 23:09:01 +0300

    cert пакет

--- internal/pkg/httpserver/httpserver.go
+++ internal/pkg/httpserver/httpserver.go
@@ -1,15 +1,12 @@
 package httpserver
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"math/big"
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/zaz600/go-musthave-shortener/internal/pkg/cert"
 )
 
 type tcpKeepAliveListener struct {
@@ -27,14 +24,14 @@ func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
 }
 
 func ListenTLS(server *http.Server, address string) error {
-	cert, err := GenCert()
+	tlsCert, err := cert.New()
 	if err != nil {
 		return err
 	}
 
 	tlsConfig := &tls.Config{}
 	tlsConfig.NextProtos = []string{"http/1.1"}
-	tlsConfig.Certificates = []tls.Certificate{cert}
+	tlsConfig.Certificates = []tls.Certificate{tlsCert}
 
 	ln, err := net.Listen("tcp", address)
 	if err != nil {
@@ -44,37 +41,4 @@ func ListenTLS(server *http.Server, address string) error {
 	tlsListener := tls.NewListener(tcpKeepAliveListener{ln.(*net.TCPListener)}, tlsConfig)
 
 	return server.Serve(tlsListener)
-}
-
-// GenCert генерирует самоподписанный сертификат
-func GenCert() (tls.Certificate, error) {
-	certTemplate := &x509.Certificate{
-		SerialNumber: big.NewInt(time.Now().Unix()),
-		Subject: pkix.Name{
-			Organization: []string{"Yandex.Praktikum"},
-			Country:      []string{"RU"},
-		},
-		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-	}
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, certTemplate, certTemplate, privateKey.Public(), privateKey)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-
-	var outCert tls.Certificate
-	outCert.Certificate = append(outCert.Certificate, certBytes)
-	outCert.PrivateKey = privateKey
-
-	return outCert, nil
 }
